13bfd5737bbe6ebc6104a3d761b93637f3f895bb
roeldev <roeldev@users.noreply.github.com>
2019-05-19 19:55:18 +0200

    chore: Download composer installer in seperate stage

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-if [[ ! -f /root/.composer/composer.json ]]; then
+if [ -f /usr/local/bin/composer ] && [ ! -f /root/.composer/composer.json ]
+then
     echo Installing global composer packages, please wait...
     mv /tmp/composer.json /root/.composer
     composer install \
